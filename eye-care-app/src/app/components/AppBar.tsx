f3c60b656d7f70788e88b0d37548b3841d80a7da
okayyjen <bkim3@unb.ca>
2024-05-29 00:44:03 -0300

    style changes to app bar

--- eye-care-app/src/app/components/AppBar.tsx
+++ eye-care-app/src/app/components/AppBar.tsx
@@ -1,62 +1,125 @@
+"use client"
 import React from "react";
-import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Link, Input } from "@nextui-org/react";
-import SearchIcon from "./SearchIcon";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, NavbarMenuToggle, Input, NavbarMenu, NavbarMenuItem, Link, Button, Dropdown, DropdownTrigger, DropdownItem, DropdownMenu } from "@nextui-org/react";
+import { SearchIcon, ChevronDown } from "./Icons";
+import { AcmeLogo } from "./AcmeLogo";
 
 const AppBar = () => {
+  const [isMenuOpen, setIsMenuOpen] = React.useState(false);
+  const menuItems = [
+    "Services",
+    "About Us",
+    "Our Team",
+    "Resources",
+    "Feedback",
+    "Contact Us",
+  ];
+
+  const icons = {
+    chevron: <ChevronDown fill="currentColor" size={16} />
+  };
+
   return (
-    <Navbar isBordered maxWidth="xl">
-      <NavbarContent justify="start">
-        <NavbarBrand className="mr-4">
-          <p className="hidden sm:block font-bold text-inherit">LOGO</p>
+    <Navbar onMenuOpenChange={setIsMenuOpen} maxWidth="xl">
+      <NavbarContent>
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+          className="sm:hidden"
+        />
+        <NavbarBrand>
+          <AcmeLogo />
+          <p className="font-bold text-inherit">ACME</p>
         </NavbarBrand>
-        <NavbarContent className="hidden sm:flex gap-6">
+      </NavbarContent>
+
+      <NavbarContent className="hidden sm:flex gap-6" justify="center">
+        <Dropdown>
           <NavbarItem>
-            <Link color="foreground" href="#">
-              SERVICES
-            </Link>
+            <DropdownTrigger>
+                <Button
+                  disableRipple
+                  className="p-0 bg-transparent data-[hover=true]:bg-transparent"
+                  endContent={icons.chevron}
+                  radius="sm"
+                  variant="light"
+                >
+                  <Link color="foreground" href="#">
+                    SERVICES
+                  </Link>
+                </Button>
+            </DropdownTrigger>
           </NavbarItem>
-          <NavbarItem>
-            <Link color="foreground" href="#">
-              ABOUT US
-            </Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link color="foreground" href="#">
-              OUR TEAM
-            </Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link color="foreground" href="#">
-              RESOURCES
-            </Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link color="foreground" href="#">
-              FEEDBACK
-            </Link>
-          </NavbarItem>
-          <NavbarItem>
-            <Link color="foreground" href="#">
-              CONTACT US
-            </Link>
-          </NavbarItem>
-        </NavbarContent>
+          <DropdownMenu
+            aria-label="Action Service"
+            className="w-[200px]"
+            itemClasses={{
+              base: "gap-4",
+            }}
+          >
+            <DropdownItem key="loremipsum">Lorem Ipsum</DropdownItem>
+            <DropdownItem key="dolor_sit">Dolor Sit</DropdownItem>
+            <DropdownItem key="amet">Amet</DropdownItem>
+          </DropdownMenu>
+        </Dropdown>
+        
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            ABOUT US
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            OUR TEAM
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            RESOURCES
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            FEEDBACK
+          </Link>
+        </NavbarItem>
+        <NavbarItem>
+          <Link color="foreground" href="#">
+            CONTACT US
+          </Link>
+        </NavbarItem>
       </NavbarContent>
 
       <NavbarContent as="div" className="items-center" justify="end">
-        <Input
-          classNames={{
-            base: "max-w-full sm:max-w-[10rem] h-10",
-            mainWrapper: "h-full",
-            input: "text-small",
-            inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
-          }}
-          placeholder="Type to search..."
-          size="sm"
-          startContent={<SearchIcon size={18} />}
-          type="search"
-        />
+          <Input
+            classNames={{
+              base: "max-w-full sm:max-w-[10rem] h-10",
+              mainWrapper: "h-full",
+              input: "text-small",
+              inputWrapper: "h-full font-normal text-default-500 bg-default-400/20 dark:bg-default-500/20",
+            }}
+            placeholder="Type to search..."
+            size="sm"
+            startContent={<SearchIcon size={18} />}
+            type="search"
+          />
       </NavbarContent>
+
+      <NavbarMenu>
+        {menuItems.map((item, index) => (
+          <NavbarMenuItem key={`${item}-${index}`}>
+            <Link
+              color={
+                "foreground"
+              }
+              className="w-full"
+              href="#"
+              size="lg"
+            >
+              {item}
+            </Link>
+          </NavbarMenuItem>
+        ))}
+      </NavbarMenu>
     </Navbar>
   );
 }
--- eye-care-app/src/app/components/Icons.tsx
+++ eye-care-app/src/app/components/Icons.tsx
@@ -7,7 +7,7 @@ interface SearchIconProps {
   height?: number;
 }
 
-const SearchIcon: React.FC<SearchIconProps> = ({
+export const SearchIcon: React.FC<SearchIconProps> = ({
   size = 24,
   strokeWidth = 1.5,
   width,
@@ -41,4 +41,31 @@ const SearchIcon: React.FC<SearchIconProps> = ({
   </svg>
 );
 
-export default SearchIcon;
+interface ChevronDownProps extends React.SVGProps<SVGSVGElement> {
+  fill?: string;
+  size?: number;
+  height?: number;
+  width?: number;
+}
+
+export const ChevronDown: React.FC<ChevronDownProps> = ({ fill = 'currentColor', size, height, width, ...props }) => {
+  return (
+    <svg
+      fill="none"
+      height={size || height || 24}
+      viewBox="0 0 24 24"
+      width={size || width || 24}
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        d="m19.92 8.95-6.52 6.52c-.77.77-2.03.77-2.8 0L4.08 8.95"
+        stroke={fill}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeMiterlimit={10}
+        strokeWidth={1.5}
+      />
+    </svg>
+  );
+};
