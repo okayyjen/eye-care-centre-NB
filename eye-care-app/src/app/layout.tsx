0593700ebee8ce8106e4cfad1ae3b92ac59732cd
Amirah <harbamirah@gmail.com>
2024-05-29 19:35:43 -0300

    added curve to footer

--- eye-care-app/src/app/layout.tsx
+++ eye-care-app/src/app/layout.tsx
@@ -25,9 +25,9 @@ export default function RootLayout({
           <AppBar />
           <main>{children}</main>
           <div id="footer-content">
-            <svg className="wave" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-              <path fill="#8695c0" fillOpacity="1" d="M0,128L60,112C120,96,240,64,360,58.7C480,53,600,75,720,101.3C840,128,960,160,1080,154.7C1200,149,1320,107,1380,85.3L1440,64L1440,320L1380,320C1320,320,1200,320,1080,320C960,320,840,320,720,320C600,320,480,320,360,320C240,320,120,320,60,320L0,320Z"></path>
-            </svg>
+              <svg className="curved-top-svg" viewBox="0 0 300 20" preserveAspectRatio="none">
+                <path d="M-50,50 A200,50 0 0,1 350,50 L300,50 L0,50 Z" fill="#8695c0"></path>
+              </svg>
             <Footer />
           </div>
         </Providers>
