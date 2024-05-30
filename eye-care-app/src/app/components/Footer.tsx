732e18edfffea7971f1dfce42bab56e9af2b6271
Amirah <harbamirah@gmail.com>
2024-05-30 19:56:42 -0300

    adding images to footer

--- eye-care-app/src/app/components/Footer.tsx
+++ eye-care-app/src/app/components/Footer.tsx
@@ -5,21 +5,35 @@ const Footer = () => {
     <div className="footer">
       <div className="main-footer">
         <div className="wrapper">
-          <div className="logo-contact-container">
-            <div className="image-container">
-              <Image className="logo"
-                src={require("./visible.png")}
-                alt="logo"
-                layout="responsive"
-              />
-            </div>
-          </div>
           <div className="contact-location">
-            <h2>Contact Us</h2>
-            <p>(506) 830-1773<br />
-              5085 Principale-Est Ave, Dieppe, NB E1A 9T8<br />
-              <a href="mailto:example@example.com">example@example.com</a>
-            </p>
+            <h2 id="contact-title">Contact Us</h2>
+            <div className="contact-info">
+              <div className='footer-info-wrapper'>
+                <div className="image-container">
+                <Image id="phone-image"
+                  src={require("../images/phone.png")}
+                  alt="logo"
+                  layout="responsive"
+                />
+                </div>
+                <div>
+                  <div id="phone-number">(506) 830-1773</div> 
+                  <div id = "email"><a href="mailto:example@example.com">example@example.com</a></div>
+                </div>
+
+              </div>
+              <div className='footer-info-wrapper'>
+                <div className="image-container" style={{width: '10%', height: '10%', position: 'relative'}}>
+                  <Image id="location-image"
+                    src={require("../images/location.png")}
+                    alt="logo"
+                    layout="responsive"
+                  />
+                </div>
+                <div id="address">5085 Principale-Est Ave, Dieppe, NB E1A 9T8</div>
+              </div>
+
+            </div>
           </div>
         </div>
         <div className="wrapper">
