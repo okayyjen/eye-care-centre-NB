b07b6e0ca545d53725e7d1562a69689870d5d347
amirah <am263157@dal.ca>
2025-02-03 21:06:38 -0500

    carousel refactor

--- eye-care-app/src/app/components/ImageCarousel.tsx
+++ eye-care-app/src/app/components/ImageCarousel.tsx
@@ -26,31 +26,32 @@ const ImageCarousel = () => {
     };
 
     return (
-        <div className="image-carousel-container">
+        <div className="p-0 max-w-[1200px] h-auto mx-auto relative">
             <div id="image-carousel">
                 <Slider {...settings} ref={sliderRef}>
                     {images.map((image, index) => (
-                        <div key={index} className="image-container">
+                        <div key={index} className="relative w-full">
                             <Image
                                 src={image.default.src}
                                 alt={`Slide ${index + 1}`}
                                 layout="responsive"
                                 width={500}
                                 height={500}
+                                className="w-full h-auto"
                             />
-                            <div className="text-overlay">
-                                <h3>Lorem Ipsum & Dolor Sit Amet</h3>
+                            <div className="absolute inset-0 flex flex-col justify-start items-start p-5 text-white bg-black/50 text-[50px] box-border">
+                                <h3 className = "w-[300px] font-extrabold pt-5 pl-[50px]">Lorem Ipsum & Dolor Sit Amet</h3>
                             </div>
                         </div>
                     ))}
                 </Slider>
             </div>
-            <div id="button-container">
-                <button className="carousel-button-prev" onClick={() => sliderRef.current.slickPrev()}>
-                    <MdOutlineArrowBackIos className="arrow-icon"/>
+            <div className="absolute top-1/2 left-0 w-full flex justify-between z-10 text-white">
+                <button className="bg-[#384C84] p-2 rounded-lg m-2" onClick={() => sliderRef.current.slickPrev()}>
+                    <MdOutlineArrowBackIos/>
                 </button>
-                <button className="carousel-button-next" onClick={() => sliderRef.current.slickNext()}>
-                    <MdOutlineArrowForwardIos className="arrow-icon"/>
+                <button className="bg-[#384C84] p-2 rounded-lg m-2" onClick={() => sliderRef.current.slickNext()}>
+                    <MdOutlineArrowForwardIos/>
                 </button>
             </div>
         </div>
