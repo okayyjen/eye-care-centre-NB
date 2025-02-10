48ab48eae64b35857463a18030c6381fed149dd2
Amirah <harbamirah@gmail.com>
2025-02-09 21:24:16 -0400

    resources.css --> tailwind

--- eye-care-app/src/app/resources/page.tsx
+++ eye-care-app/src/app/resources/page.tsx
@@ -1,27 +1,21 @@
-import "../resources.css"
-import Image from 'next/image'
-
 export default function ResourcePage(){
     return(
-        <div className="resource-page-wrapper">
-
-            <div className="resource-container">
-                <h1 id="resource-title">LOREM IPSUM</h1>
+        <div className="flex justify-center w-full bg-white pb-10">
+            <div className="w-full max-w-screen-xl text-left pt-5">
+                <h1 className="font-bold text-lg">LOREM IPSUM</h1>
                 <div className="separator">_________</div>
-                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                <h2>Lorem Ipsum</h2>
-                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                <h2>Lorem Ipsum</h2>
-                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                <h2>Lorem Ipsum</h2>
-                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                <h2>Lorem Ipsum</h2>
-                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-                <h2>Lorem Ipsum</h2>
-                <p>Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
-
+                <p className="pb-5">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                <h2 className="font-bold text-lg">Lorem Ipsum</h2>
+                <p className="pb-5">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                <h2 className="font-bold text-lg">Lorem Ipsum</h2>
+                <p className="pb-5">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                <h2 className="font-bold text-lg">Lorem Ipsum</h2>
+                <p className="pb-5">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                <h2 className="font-bold text-lg">Lorem Ipsum</h2>
+                <p className="pb-5">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
+                <h2 className="font-bold text-lg">Lorem Ipsum</h2>
+                <p className="pb-5">Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi architecto beatae vitae dicta sunt explicabo.</p>
             </div>
-            
         </div>
         
     )
