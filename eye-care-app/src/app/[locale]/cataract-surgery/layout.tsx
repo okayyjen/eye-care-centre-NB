3f267f7c74b34d6d5da237a05d96c302834ee24d
Amirah <harbamirah@gmail.com>
2025-02-16 19:22:31 -0400

    cataract surgery page

---
+++ eye-care-app/src/app/[locale]/cataract-surgery/layout.tsx
@@ -0,0 +1,11 @@
+export default function CataractSurgeryPageLayout({
+    children,
+  }: {
+    children: React.ReactNode
+  }) {
+    return (
+      <section>
+        {children}
+      </section>
+    )
+  }
