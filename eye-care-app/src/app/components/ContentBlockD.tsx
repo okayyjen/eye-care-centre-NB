189c39ba41a0a47f6803cc1aa485914cb6cc30f6
Amirah <harbamirah@gmail.com>
2025-07-22 20:32:36 -0300

    adding meet our team button link

--- eye-care-app/src/app/components/ContentBlockD.tsx
+++ eye-care-app/src/app/components/ContentBlockD.tsx
@@ -1,8 +1,13 @@
+"use client";
 import React from 'react';
 import DoctorCard from './DoctorCard';
 import { DoctorCardsData } from '../data/general';
+import Link from 'next/link';
+import { usePathname, useParams } from "next/navigation";
 
 const ContentBlockD = () => {
+    const params = useParams();
+    const currentLocale = params.locale || "en";
     return (
         <div className="p-[70px] text-center">
             <h2 className="pb-5 text-xl">A NETWORK OF EXPERIENCED DOCTORS</h2>
@@ -14,7 +19,12 @@ const ContentBlockD = () => {
                 ))}
 
             </div>
-            <button className="mt-10 border-3 border-none w-auto py-4 px-7 rounded-[5px] bg-[#8695C0] text-white">MEET OUR TEAM</button>
+
+            <button className="mt-10 border-3 border-none w-auto py-4 px-7 rounded-[5px] bg-[#8695C0] text-white">            
+                <Link href={`/${currentLocale}/our-team`} className=" hover:no-underline">
+                    MEET OUR TEAM
+                </Link>
+            </button>
 
 
         </div>
