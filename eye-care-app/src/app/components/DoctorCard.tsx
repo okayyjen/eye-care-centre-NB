8a0c891f0fddd8bd73cf75e7539d39408eeb5e6e
Amirah <harbamirah@gmail.com>
2025-07-22 23:01:59 -0300

    doctors section size down

--- eye-care-app/src/app/components/DoctorCard.tsx
+++ eye-care-app/src/app/components/DoctorCard.tsx
@@ -9,13 +9,15 @@ interface DoctorCardProps {
 const DoctorCard: React.FC<DoctorCardProps> = ({ name, text }) => {
   return (
     <div className="max-w-[350px] m-2.5 box-border text-center">
-        <Image className="w-[50px] h-auto"
-            src={require("../images/doctor.png")}
-            alt="logo"
-            layout="responsive"
-        />
-        <h1 className="pt-2 text-[#384C84] font-bold pb-5 text-xl">{name}</h1>
-        <p>{text}</p>
+      <Image
+        className="h-[300px] w-auto mx-auto"
+        src={require("../images/doctor.png")}
+        alt="doctor"
+        height={100}
+        width={0}
+      />
+      <h1 className="pt-2 text-[#384C84] font-bold pb-5 text-xl">{name}</h1>
+      <p>{text}</p>
     </div>
   );
 };
