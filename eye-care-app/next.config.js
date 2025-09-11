2033ffe579c24625344a79987536d8d22d5957c2
okayyjen <bkim3@unb.ca>
2025-09-10 22:51:45 -0300

    converting config file to commonjs

---
+++ eye-care-app/next.config.js
@@ -0,0 +1,8 @@
+const createNextIntlPlugin = require("next-intl/plugin");
+const withNextIntl = createNextIntlPlugin();
+
+const nextConfig = {
+  reactStrictMode: true,
+};
+
+module.exports = withNextIntl(nextConfig);
